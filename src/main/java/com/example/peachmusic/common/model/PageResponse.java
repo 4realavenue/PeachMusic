fcf7ff2802d13e09248cc453c7c5b0807641320e
4realavenue <4realdane@gmail.com>
2026-01-15 21:33:20 +0900

    ✨ feat: V1 브랜치 생성
    - V1 브랜치 생성

--- src/main/java/com/example/peachmusic/common/model/PageResponse.java
+++ src/main/java/com/example/peachmusic/common/model/PageResponse.java
@@ -8,7 +8,6 @@ import java.util.List;
 @Getter
 public class PageResponse<T> {
 
-    // ass
     private final List<T> content;
     private final long totalElements;
     private final int totalPages;
