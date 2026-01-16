0541a3daa11bce97a0ca7867fa8b5954ba246f65
4realavenue <4realdane@gmail.com>
2026-01-16 13:36:04 +0900

    ♻ refactor: 공통 페이지 응답 객체 수정
    - 공통 응답 객체로 사용 가능하도록 변경
    - 내부 클래스가 페이지 데이터 반환도록 변경
    - 데이터가 없는 성공 응답 추가
    - 데이터가 포함 된 성공 응답 추가
    - 데이터가 없는 실패 응답 추가
    - 데이터가 포함 된 실패 응답 추가

--- src/main/java/com/example/peachmusic/common/model/PageResponse.java
+++ src/main/java/com/example/peachmusic/common/model/PageResponse.java
@@ -1,34 +1,68 @@
 package com.example.peachmusic.common.model;
 
+import com.example.peachmusic.common.exception.ErrorCode;
 import lombok.Getter;
 import org.springframework.data.domain.Page;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
 public class PageResponse<T> {
 
-    private final List<T> content;
-    private final long totalElements;
-    private final int totalPages;
-    private final int size;
-    private final int number;
+    private final boolean success;
+    private final String message;
+    private final PageData<T> data;
+    private final LocalDateTime timestamp;
 
-    private PageResponse(List<T> content, long totalElements, int totalPages, int size, int number) {
-        this.content = content;
-        this.totalElements = totalElements;
-        this.totalPages = totalPages;
-        this.size = size;
-        this.number = number;
+    public PageResponse(boolean success, String message, PageData<T> data) {
+        this.success = success;
+        this.message = message;
+        this.data = data;
+        this.timestamp = LocalDateTime.now();
     }
 
-    public static <T> PageResponse<T> from(Page<T> page) {
-        return new PageResponse<>(
-                page.getContent(),
-                page.getTotalElements(),
-                page.getTotalPages(),
-                page.getSize(),
-                page.getNumber()
-        );
+
+    // 성공 응답 (응답 데이터 없음)
+    public static PageResponse<Void> success(String message) {
+        return new PageResponse<>(true, message, null);
+    }
+
+    // 성공 응답 (응답 데이터 포함)
+    public static <T> PageResponse<T> success(String message, Page<T> page) {
+        return new PageResponse<>(true, message, new PageData<>(page));
+    }
+
+    // 실패 응답 (응답 데이터 없음)
+    public static PageResponse<Void> fail(ErrorCode errorCode) {
+        return new PageResponse<>(false, errorCode.getMessage(), null);
+    }
+
+    // 실패 응답 (응답 데이터 포함)
+    public static <T> PageResponse<T> fail(ErrorCode errorCode, Page<T> page) {
+        return new PageResponse<>(false, errorCode.getMessage(), new PageData<>(page));
+    }
+
+
+    /**
+     * 내부 클래스
+     * @param <T>
+     */
+    @Getter
+    public static class PageData<T> {
+
+        private final List<T> content;
+        private final long totalElements;
+        private final int totalPages;
+        private final int size;
+        private final int number;
+
+        public PageData(Page<T> page) {
+            this.content = page.getContent();
+            this.totalElements = page.getTotalElements();
+            this.totalPages = page.getTotalPages();
+            this.size = page.getSize();
+            this.number = page.getNumber();
+        }
     }
 }
