b911542ceeb7b3ee10cff6414f6be619bb7d51b2
4realavenue <4realdane@gmail.com>
2026-01-16 13:38:09 +0900

    ♻ refactor: 전역 예외처리 핸들러 수정
    - 메서드 설명 주석 추가
    - 공통 페이지 응답 예외처리 핸들러 추가

--- src/main/java/com/example/peachmusic/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/peachmusic/common/exception/GlobalExceptionHandler.java
@@ -1,9 +1,9 @@
 package com.example.peachmusic.common.exception;
 
 import com.example.peachmusic.common.model.CommonResponse;
+import com.example.peachmusic.common.model.PageResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -14,6 +14,7 @@ import java.util.Objects;
 @Slf4j
 public class GlobalExceptionHandler {
 
+    // Valid 예외 - 공통 응답
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<CommonResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
 
@@ -24,11 +25,22 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(ex.getStatusCode()).body(new CommonResponse(false, message, null));
     }
 
+    // Custom 예외 - 공통 응답
     @ExceptionHandler(value = CustomException.class)
     public ResponseEntity<CommonResponse> handlerCustomException(CustomException ex) {
 
         log.error("CustomException 발생 : {} ", ex.getMessage());
 
-        return ResponseEntity.status(ex.getErrorCode().getStatus()).body(new CommonResponse<>(false, ex.getErrorCode().getMessage(), null));
+        return ResponseEntity.status(ex.getErrorCode().getStatus()).body(CommonResponse.fail(ex.getErrorCode()));
+    }
+
+    // Custom 예외 - 공통 페이지 응답
+    @ExceptionHandler(value = CustomException.class)
+    public ResponseEntity<PageResponse> handlerCustomPageException(CustomException ex) {
+
+        log.error("CustomException 발생 : {} ", ex.getMessage());
+
+        return ResponseEntity.status(ex.getErrorCode().getStatus()).body(PageResponse.fail(ex.getErrorCode()));
+
     }
 }
