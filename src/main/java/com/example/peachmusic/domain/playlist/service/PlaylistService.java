013d7f515f602b5f40052e0e6fd979d4dcf0521e
4realavenue <4realdane@gmail.com>
2026-01-22 14:56:29 +0900

    💄 style: 플레이리스트 주석 제거
    - 불필요한 주석 제거

--- src/main/java/com/example/peachmusic/domain/playlist/service/PlaylistService.java
+++ src/main/java/com/example/peachmusic/domain/playlist/service/PlaylistService.java
@@ -35,20 +35,15 @@ public class PlaylistService {
     @Transactional
     public PlaylistCreateResponseDto createPlaylist(PlaylistCreateRequestDto requestDto, AuthUser authUser) {
 
-        // 1. 로그인 한 유저의 id 받아오기
         Long userId = authUser.getUserId();
 
-        // 2. 로그인한 유저의 id로 유저 찾아오기
         User findUser = userRepository.findById(userId)
                 .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
 
-        // 3. 새로운 플레이리스트 생성
         Playlist playlist = new Playlist(findUser, requestDto.getPlaylistName(), requestDto.getPlaylistImage());
 
-        // 4. 생성 한 플레이리스트 저장
         playlistRepository.save(playlist);
 
-        // 5. 응답dto의 from 메서드 실행 후 반환
         return PlaylistCreateResponseDto.from(playlist);
 
     }
@@ -59,18 +54,13 @@ public class PlaylistService {
     @Transactional(readOnly = true)
     public List<PlaylistGetListResponseDto> getPlaylistAll(AuthUser authUser) {
 
-        // 1. 로그인 한 유저의 id 받아오기
         Long userId = authUser.getUserId();
 
-        // 2. 로그인 한 유저의 id로 유저 찾아오기
         User findUser = userRepository.findById(userId)
                 .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
 
-        // 3. 찾아온 유저로 플레이리스트 리스트에 담아서 찾아옴
         List<Playlist> findPlaylistList = playlistRepository.findAllByUser(findUser);
 
-        // 4. 찾아온 플레이리스트 리스트를 순회하면서 응답dto의 from 메서드를 실행 시키고
-        //    리스트화 한 데이터를 반환
         return findPlaylistList.stream().map(PlaylistGetListResponseDto::from).toList();
 
     }
@@ -81,26 +71,19 @@ public class PlaylistService {
     @Transactional(readOnly = true)
     public PlaylistGetSongResponseDto getPlaylistSongList(Long playlistId, AuthUser authUser) {
 
-        // 1. 로그인 한 유저의 id 받아오기
         Long userId = authUser.getUserId();
 
-        // 2. 플레이리스트 아이디로 플레이리스트 찾아옴
         Playlist findPlaylist = playlistRepository.findById(playlistId)
                 .orElseThrow(() -> new CustomException(ErrorCode.PLAYLIST_NOT_FOUND));
 
-        // 3. 찾아온 플레이리스트의 유저 아이디가 로그인 한 유저와 일치하지 않다면 예외처리
         if (!findPlaylist.getUser().getUserId().equals(userId)) {
             throw new CustomException(ErrorCode.AUTH_AUTHORIZATION_REQUIRED);
         }
 
-        // 4. 플레이리스트가 갖고 있는 음원들 리스트에 담아옴
         List<PlaylistSong> findPlaylistSong = playlistSongRepository.findAllByPlaylist(findPlaylist);
 
-        // 5. 음원들 리스트를 순회하면서 응답dto의 내부 클래스인 응답 데이터 dto의 from 메서드를 실행 시키고
-        //    응답 데이터 dto 리스트에 담아줌
         List<PlaylistGetSongResponseDto.SongResponseDto> playlistSongDtoList = findPlaylistSong.stream().map(PlaylistGetSongResponseDto.SongResponseDto::from).toList();
 
-        // 6. 응답dto의 메서드 from 메서드 실행 후 반환
         return PlaylistGetSongResponseDto.from(findPlaylist, playlistSongDtoList);
 
     }
@@ -111,23 +94,17 @@ public class PlaylistService {
     @Transactional
     public PlaylistUpdateResponseDto updatePlaylist(Long playlistId, PlaylistUpdateRequestDto requestDto, AuthUser authUser) {
 
-        // 1. 로그인 한 유저의 id 받아오기
         Long userId = authUser.getUserId();
 
-        // 2. 요청 받은 플레이리스트 아이디로 플레이리스트 찾아오기
         Playlist findPlaylist = playlistRepository.findById(playlistId)
                 .orElseThrow(() -> new CustomException(ErrorCode.PLAYLIST_NOT_FOUND));
 
-        // 3. 찾아온 플레이리스트의 유저 아이디가 로그인 한 유저와 일치하지 않다면 예외처리
         if (!findPlaylist.getUser().getUserId().equals(userId)) {
             throw new CustomException(ErrorCode.AUTH_AUTHORIZATION_REQUIRED);
         }
 
-        // 4. 찾아온 플레이리스트의 updatePlaylist 메서드 실행
-        //    RequestBody로 받아온 playlistName으로 갈아 끼우기
         findPlaylist.updatePlaylist(requestDto);
 
-        // 5. 응답dto의 from 메서드 실행 후 반환
         return PlaylistUpdateResponseDto.from(findPlaylist);
 
     }
@@ -138,23 +115,17 @@ public class PlaylistService {
     @Transactional
     public void deletePlaylist(Long playlistId, AuthUser authUser) {
 
-        // 1. 로그인 한 유저의 id 받아오기
         Long userId = authUser.getUserId();
 
-        // 2. 요청 받은 플레이리스트 아이디로 플레이리스트 찾아오기
         Playlist findPlaylist = playlistRepository.findById(playlistId)
                 .orElseThrow(() -> new CustomException(ErrorCode.PLAYLIST_NOT_FOUND));
 
-        // 3. 찾아온 플레이리스트의 유저 아이디가 로그인 한 유저와 일치하지 않다면 예외처리
         if (!findPlaylist.getUser().getUserId().equals(userId)) {
             throw new CustomException(ErrorCode.AUTH_AUTHORIZATION_REQUIRED);
         }
 
-        // 4. 연관관계로 묶여있어서 플레이리스트가 바로 삭제 안됨
-        //    찾아온 플레이리스트에 있는 음원들 전부 삭제
         playlistSongRepository.deleteAllByPlaylist(findPlaylist);
 
-        // 3. 찾아온 플레이리스트 삭제
         playlistRepository.delete(findPlaylist);
 
     }
