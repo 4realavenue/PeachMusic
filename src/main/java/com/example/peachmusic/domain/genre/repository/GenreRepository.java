07db8253bb8a04507be686267e002abd910367c5
sseung0510 <choiseunghi0510@gmail.com>
2026-01-23 15:57:36 +0900

    💄 style: 안쓰는 import문 삭제
    1. 안쓰는 import문 삭제

--- src/main/java/com/example/peachmusic/domain/genre/repository/GenreRepository.java
+++ src/main/java/com/example/peachmusic/domain/genre/repository/GenreRepository.java
@@ -2,10 +2,6 @@ package com.example.peachmusic.domain.genre.repository;
 
 import com.example.peachmusic.domain.genre.entity.Genre;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-import java.util.List;
-import java.util.Optional;
 
 public interface GenreRepository extends JpaRepository<Genre, Long> {
 }
