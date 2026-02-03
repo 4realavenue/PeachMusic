b3cca9a77425cb1b23c832511dd2c8d3a7440d94
4realavenue <4realdane@gmail.com>
2026-02-03 10:02:28 +0900

    ✨ feat: 음원 작업 상태 테이블 추가
    - 음원 작업 상태 테이블 생성 DDL 추가
    - 음원 작업 상태 ENUM 추가
    - 음원 작업 상테 Entity 추가
    - 음원 작업 상태 Repository 추가

---
+++ src/main/java/com/example/peachmusic/common/enums/JobStatus.java
@@ -0,0 +1,20 @@
+package com.example.peachmusic.common.enums;
+
+public enum JobStatus {
+
+    NOT_READY,
+
+    DOWNLOADING,
+
+    DOWNLOAD_FAILED,
+
+    READY,
+
+    TRANSCODING,
+
+    TRANSCODE_FAILED,
+
+    SUCCESS
+
+    ;
+}
---
+++ src/main/java/com/example/peachmusic/domain/streamingjob/entity/StreamingJob.java
@@ -0,0 +1,33 @@
+package com.example.peachmusic.domain.streamingjob.entity;
+
+import com.example.peachmusic.common.enums.JobStatus;
+import com.example.peachmusic.domain.song.entity.Song;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "streaming_jobs")
+public class StreamingJob {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "streaming_job_id")
+    private Long streamingJobId;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "song_id", nullable = false)
+    private Song song;
+
+    @Enumerated(value = EnumType.STRING)
+    @Column(name = "job_status")
+    private JobStatus jobStatus;
+
+    public StreamingJob(Song song, JobStatus jobStatus) {
+        this.song = song;
+        this.jobStatus = jobStatus;
+    }
+}
