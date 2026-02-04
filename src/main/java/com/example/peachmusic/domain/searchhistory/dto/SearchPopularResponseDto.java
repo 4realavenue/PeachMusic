0d1d0a609a3fbf9c5d57f60b6cd94bb30b4864ea
minss22 <kms141516@gmail.com>
2026-02-04 21:56:43 +0900

    ✨ feat: 인기검색어를 Redis zset을 이용하도록 수정
    - 날짜+시간마다 검색어 횟수 기록
    - SearchHistory Entity, Repository 삭제
    - sql 수정

--- src/main/java/com/example/peachmusic/domain/searchhistory/dto/SearchPopularResponseDto.java
+++ src/main/java/com/example/peachmusic/domain/searchhistory/dto/SearchPopularResponseDto.java
@@ -2,6 +2,7 @@ package com.example.peachmusic.domain.searchhistory.dto;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.ZSetOperations.TypedTuple;
 
 @Getter
 @RequiredArgsConstructor
@@ -10,4 +11,8 @@ public class SearchPopularResponseDto {
     private final int rank;
     private final String keyword;
     private final Long count;
+
+    public static SearchPopularResponseDto of(int rank, TypedTuple<String> tuple) {
+        return new SearchPopularResponseDto(rank, tuple.getValue(), tuple.getScore().longValue());
+    }
 }
--- src/main/java/com/example/peachmusic/domain/searchhistory/repository/SearchHistoryCustomRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.peachmusic.domain.searchhistory.repository;
-
-import com.example.peachmusic.domain.searchhistory.dto.SearchPopularResponseDto;
-import java.util.List;
-
-public interface SearchHistoryCustomRepository {
-    List<SearchPopularResponseDto> findPopularKeyword();
-}
