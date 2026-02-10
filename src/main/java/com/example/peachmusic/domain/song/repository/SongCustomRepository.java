5349bb6a6ad14924b23baa84d88d89da731e5089
oezy-coder <a8609556@gmail.com>
2026-02-10 16:15:22 +0900

    ♻ refactor: SongCustomRepository 쿼리 메서드 정리

--- src/main/java/com/example/peachmusic/domain/song/repository/SongCustomRepository.java
+++ src/main/java/com/example/peachmusic/domain/song/repository/SongCustomRepository.java
@@ -13,9 +13,8 @@ public interface SongCustomRepository {
     List<SongSearchResponseDto> findSongKeysetPageByWord(String word, int size, boolean isAdmin, SortType sortType, SortDirection direction, CursorParam cursor);
     List<SongSearchResponseDto> findSongListByWord(String word, int size, boolean isAdmin, SortType sortType, SortDirection direction);
     List<SongArtistDetailResponseDto> findSongList(Long userId, Long artistId, int size);
-    List<SongArtistDetailResponseDto> findSongByArtistKeyset(Long userId, Long artistId, SortType sortType, SortDirection sortDirection, Long lastId, LocalDate lastDate, int size);
+    List<SongArtistDetailResponseDto> findSongByArtistKeyset(Long userId, Long artistId, SortType sortType, SortDirection sortDirection, CursorParam cursor, int size);
 
     List<SongSummaryDto> findSongSummaryListByAlbumId(Long albumId);
-    List<SongArtistDetailResponseDto> findSongByArtistKeyset(Long userId, Long artistId, SortType sortType, SortDirection sortDirection, CursorParam cursor, int size);
 }
 
