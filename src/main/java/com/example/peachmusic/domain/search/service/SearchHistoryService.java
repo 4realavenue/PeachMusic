c7f9862b09b78a0a7b87345ddd8eb1f84ea324af
minss22 <kms141516@gmail.com>
2026-02-15 01:50:50 +0900

    ♻ refactor: 인기 검색어 조회 수정
    - 키가 없어야 다시 랭킹 생성하도록 수정
    - @Transactional 삭제
    - 인기검색어 랭키 키값 변경
    - 랭킹 리미트 상수로 추가

--- src/main/java/com/example/peachmusic/domain/search/service/SearchHistoryService.java
+++ src/main/java/com/example/peachmusic/domain/search/service/SearchHistoryService.java
@@ -5,7 +5,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ZSetOperations.TypedTuple;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
@@ -19,17 +18,20 @@ import java.util.concurrent.TimeUnit;
 public class SearchHistoryService {
 
     private final RedisTemplate<String, String> redisTemplate;
-    private static final String SEARCH_KEY = "searchWord:";
+    private static final int TOP_RANK_LIMIT = 10;
+    private static final String SEARCH_RANK_KEY = "search:rank:";
 
     /**
-     * Redis zset 사용하여 날짜+시간마다 검색어 횟수 기록
+     * 검색어 랭킹 기록
+     * - Redis zset 사용하여 날짜+시간마다 검색어 횟수 기록
      * - TTL을 설정하여 24시간 뒤면 삭제됨
      * @param word 검색어
      */
-    public void recordSearch(String word) {
+    public void recordSearchRank(String word) {
 
         // 매 시간마다 검색어 랭킹 기록
-        String key = SEARCH_KEY + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd-HH"));
+        String key = SEARCH_RANK_KEY + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd-HH"));
+
         if (!redisTemplate.hasKey(key)) {
             redisTemplate.opsForZSet().add(key, word, 1);
             redisTemplate.expire(key, 24, TimeUnit.HOURS); // 24시간 뒤면 삭제
@@ -41,18 +43,19 @@ public class SearchHistoryService {
     /**
      * 최근 24시간 인기 검색어 TOP 10 조회
      */
-    @Transactional(readOnly = true)
     public List<SearchPopularResponseDto> searchPopular() {
 
-        // 최근 24시간 동안의 랭킹 만들기
-        String key = SEARCH_KEY + "last24hours";
-        List<String> keyList = getLast24HourKeyList();
-        redisTemplate.opsForZSet().unionAndStore(keyList.get(0), keyList.subList(1, keyList.size()), key);
-        redisTemplate.expire(key, 3, TimeUnit.MINUTES);
+        String key = SEARCH_RANK_KEY + "last24hours";
 
-        Set<TypedTuple<String>> tupleSet = redisTemplate.opsForZSet().reverseRangeWithScores(key, 0, 9);
+        if (!redisTemplate.hasKey(key)) { // 키가 없으면 → 최근 24시간 동안의 랭킹 생성
+            List<String> keyList = getLast24HourKeyList();
+            redisTemplate.opsForZSet().unionAndStore(keyList.get(0), keyList.subList(1, keyList.size()), key);
+            redisTemplate.expire(key, 5, TimeUnit.MINUTES);
+        }
 
-        if (tupleSet == null) {
+        Set<TypedTuple<String>> tupleSet = redisTemplate.opsForZSet().reverseRangeWithScores(key, 0, TOP_RANK_LIMIT-1);
+
+        if (tupleSet == null || tupleSet.isEmpty()) {
             return Collections.emptyList();
         }
 
@@ -75,7 +78,7 @@ public class SearchHistoryService {
 
         for (int i = 0; i < 24; i++) {
             LocalDateTime time = now.minusHours(i);
-            String key = SEARCH_KEY + time.format(DateTimeFormatter.ofPattern("yyyy-MM-dd-HH"));
+            String key = SEARCH_RANK_KEY + time.format(DateTimeFormatter.ofPattern("yyyy-MM-dd-HH"));
             keyList.add(key);
         }
         return keyList;
